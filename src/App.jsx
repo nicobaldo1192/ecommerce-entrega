24bea5f869b0d79690591dfc35e097a19bf4eea5
solana_grossi@hotmail.com <solana_grossi@hotmail.com>
2025-03-23 17:12:17 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import ItemDetail from "./components/pages/itemDetail/ItemDetail";
 import { ItemListContainer } from "./components/pages/itemListContainer/ItemListContainer";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { CartContextProvider } from "./context/CartContext";
+import { Toaster } from "sonner";
+
 function App() {
   return (
     <BrowserRouter>
+      <Toaster />
       <CartContextProvider>
         <Navbar />
         <Routes>

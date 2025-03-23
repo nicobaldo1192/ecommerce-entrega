c6748f4e3d8c6112faafcc4ed3465c24fec9a49e
solana_grossi@hotmail.com <solana_grossi@hotmail.com>
2025-03-23 16:42:26 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Navbar } from "./components/layouts/navbar/Navbar";
 import Cart from "./components/pages/cart/Cart";
-import Checkout from "./components/pages/Checkout/Checkout";
+import Checkout from "./components/pages/checkout/Checkout";
 import ItemDetail from "./components/pages/itemDetail/ItemDetail";
-import { ItemListContainer } from "./components/pages/ItemListContainer/ItemListContainer";
+import { ItemListContainer } from "./components/pages/itemListContainer/ItemListContainer";
 
 import { BrowserRouter, Routes, Route } from "react-router";
 import { CartContextProvider } from "./context/CartContext";
+import { Toaster } from "sonner";
 function App() {
   return (
     <BrowserRouter>
+      <Toaster richColors duration={2000} />
       <CartContextProvider>
         <Navbar />
         <Routes>
@@ -17,6 +19,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/itemDetail/:id" element={<ItemDetail />} />
+
           <Route path="*" element={<h2>404 Not found</h2>} />
         </Routes>
       </CartContextProvider>
